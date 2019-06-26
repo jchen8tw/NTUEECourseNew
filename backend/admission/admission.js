942330d9e9b107cf585076fc5e637eb966b8a446
Arvin Liu <a127000555@gmail.com>
2019-06-27 01:36:20 +0800

    split to module

--- backend/admission/admission.js
+++ backend/admission/admission.js
@@ -1,33 +1,3 @@
-/*
-B06-B03各100人
-每個人有三個志願（也就是三個老師）
-每個人要選兩門課
-
-input:
-    wish: 
-        {學號：｛
-        第一門課：［志願序］
-        第二門課：［志願序］
-        ｝}
-
-    class_info:
-        {
-            第一門課：［{Name: --, Max: --} ］,
-        }
-output 1:
-    ｛第一門課：｛
-    老師一:［學生］
-    老師二: [], 
-    +第二門課：｛｝｝
-output 2:
-    {學號：｛
-        wish + ' ' + 第一門課：［志願序］
-        wish + ' ' + 第二門課：［志願序］
-        第一門課 : 被分發到的老師,
-        第二門課 : 被分發到的老師 (nobe if empty)
-    ｝}
-*/
-
 // Random from normal distribution with std & mean
 // reference : https://stackoverflow.com/questions/25582882/javascript-math-random-normal-distribution-gaussian-bell-curve
 function randn(mean,std) {
@@ -38,56 +8,6 @@ function randn(mean,std) {
     num = num * std + mean; // Translate to 0 -> 1
     return num;
 }
-
-// random generate testcase with input format mentioned.
-function random_testcase(class_info){
-    function shuffle(a) {
-        for (let i = a.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [a[i], a[j]] = [a[j], a[i]];
-        }
-        return a;
-    }
-    test_data = {}
-    for(let year=3; year<=6; year++){
-        for(let number=1; number<=100; number++){
-            let school_number = "b0" + year + "901" + String(number).padStart(3,"0");
-            test_data[school_number] = {};
-            for(let key in class_info){
-                test_data[school_number][key] = shuffle(class_info[key].map((e)=>(e['teacher_name'])));
-            }
-        }
-    }
-    return test_data
-}
-// random_testcase but with someone not choose the class or order is not completed.
-function random_testcase2(class_info){
-    function shuffle(a) {
-        for (let i = a.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [a[i], a[j]] = [a[j], a[i]];
-        }
-        return a;
-    }
-    test_data = {}
-    for(let year=3; year<=6; year++){
-        for(let number=1; number<=100; number++){
-            let school_number = "b0" + year + "901" + String(number).padStart(3,"0");
-            test_data[school_number] = {};
-            for(let key in class_info){
-                if(Math.random() > 0.1){
-                    test_data[school_number][key] = shuffle(class_info[key].map((e)=>(e['teacher_name'])));
-                    if(Math.random() < 0.1){
-                        test_data[school_number][key] = test_data[school_number][key].slice(0,2);
-                    }
-                
-                }
-            }
-        }
-    }
-    return test_data
-}
-
 function admission(wish_ref,class_info){
     // copy to a new object.
     let wish = Object.assign({}, wish_ref);
@@ -169,31 +89,5 @@ function admission(wish_ref,class_info){
     // data  = { {num:{course_name:[teacher_list] } } }
     return [per_class, per_stu];
 }
-// This is the example class info.
-let class_info = {
-    "class 1":
-        [ 
-            {teacher_name : 'T1-1', max: 10},
-            {teacher_name : 'T1-2', max: 50},
-            {teacher_name : 'T1-3', max: 1000},
-        ],
-    "class 2":
-        [ 
-            {teacher_name : 'T2-1', max: 10},
-            {teacher_name : 'T2-2', max: 50},
-            {teacher_name : 'T2-3', max: 110},
-        ]
-};
 
-let wish = random_testcase2(class_info);
-let [per_class_result, per_stu_result] = admission(wish,class_info);
-const ordered = {};
-Object.keys(per_stu_result).sort().forEach(function(key) {
-  ordered[key] = per_stu_result[key];
-});
-per_stu_result = ordered;
-
-
-// Testing Output
-console.log(per_stu_result);
-console.log(per_class_result['class 1']['T1-1']);
+exports.admission = admission;
