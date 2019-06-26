37f266d479458af0c1dee7bdca60c7dee4d15f4e
hungchun0201 <penguin7296@gmail.com>
2019-06-26 14:27:58 +0800

    Fix style

--- frontend/src/Container/Comment.jsx
+++ frontend/src/Container/Comment.jsx
@@ -4,14 +4,25 @@ import style from './Comment.module.css';
 class Comment extends Component {
   render() {
     return (
-      <div>
-        <div id={style.commentHeader} className="jumbotron">
-          <div className="container text-center wow fadeInUp">
-            <h1 className={style.commentHeaderWord}>NTUEE 課程地圖</h1>
+      //   <div style={{ display: 'flex', flexFlow: 'column nowrap', overflowY: 'initial' }}>
+      <>
+        <div id={style.header}>
+          <div className="text-center wow fadeInUp">
+            <h1 className={style.headerWord}>NTUEE 課程地圖</h1>
             <p>加入電機 共創奇蹟</p>
           </div>
         </div>
-      </div>
+        <div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Tortor
+            dignissim convallis aenean et tortor at. Ut enim blandit volutpat
+            maecenas volutpat blandit aliquam etiam erat. Aliquam sem fringilla
+            ut morbi tincidunt augue interdum velit. Cursus metus aliquam
+            eleifend mi in nulla posuere. Lorem ipsum dolor sit amet consectetur
+          </p>
+        </div>
+      </>
     );
   }
 }
