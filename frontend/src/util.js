c1cc1f09181547577c02b6a495d72537c9132dc8
jchen8tw <11806483+jchen8tw@users.noreply.github.com>
2019-06-29 23:26:00 +0800

    fix schema change error

--- frontend/src/util.js
+++ frontend/src/util.js
@@ -20,7 +20,8 @@ export function getGrade(studentID) {
 
 const grades = ['大一', '大二', '大三\n大四', '十選二實驗'];
 export function courseToURL(course) {
-  return `/select/${grades[course.grade - 1]}/${course.name.replace(
+  console.log(course);
+  return `/select/${grades[course.grade - 1]}/${course.course_name.replace(
     /[()]/g,
     ''
   )}`;
