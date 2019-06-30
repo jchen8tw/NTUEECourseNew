76f567a56f827fb0cc3cc0fe0029ddc46a76c7c2
hungchun0201 <penguin7296@gmail.com>
2019-06-30 17:12:31 +0800

    delete redundant files

--- backend/courseComment/readFile.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-df = pd.read_excel("./originFile/電機課程地圖105-2.xlsx", sheetname=["選修", "十選二"])
-print(df)
--- backend/src/courseComment/getComment.py
+++ backend/src/courseComment/getComment.py
@@ -5,7 +5,7 @@ def storeJSON(year, header_num, skiprows_num, sheetList):
     dfs = pd.read_excel("./originFile/電機課程地圖{}.xlsx".format(year), index_col=None, header=header_num, skiprows=[skiprows_num],
                         sheet_name=sheetList)
     for _, (key, df) in enumerate(dfs.items()):
-        with open('./{}_{}.json'.format(year, key), 'w', encoding='utf-8'):
+        with open('./JSONFiles/{}_{}.json'.format(year, key), 'w', encoding='utf-8'):
             df.to_json('./JSONFiles/{}_{}.json'.format(year, key),
                        force_ascii=False, orient='records')
 
