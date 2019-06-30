5bf1d1149237766b4f4f4295d8126a90463528e9
tzyLee <b06901145@ntu.edu.tw>
2019-06-30 13:24:23 +0800

    Fix priority

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -183,7 +183,6 @@ const Mutation = {
       res.priority = [];
       return res;
     }
-    priority.sort(); // In-place, lexicographical order sort
     let wish = await Wish.findOne({ student_ids: student_id, course_name });
     if (wish === null)
       wish = new Wish({ student_ids: [student_id], course_name, priority });
